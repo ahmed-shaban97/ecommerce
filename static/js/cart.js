89b4badbe26006da8c1d46dac23886a775436c55
ahmed <ahmedcs@gmail.com>
2025-04-10 14:24:46 +0200

    add the cart total to the cart icon

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,29 +1,38 @@
-// var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart')
 
-// for (i = 0; i < updateBtns.length; i++) {
-// 	updateBtns[i].addEventListener('click', function(){
-// 		var productId = this.dataset.product
-// 		var action = this.dataset.action
-// 		console.log('productId:', productId, 'Action:', action)
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
 
-// 	})
-// }
-
-document.addEventListener("DOMContentLoaded", function() {
-    var updateBtns = document.getElementsByClassName('update-cart');
-    for (let i = 0; i < updateBtns.length; i++) {
-        updateBtns[i].addEventListener('click', function(){
-            var productId = this.dataset.product;
-            var action = this.dataset.action;
-            console.log('productId:', productId, 'Action:', action);
-            console.log('USER:', user)
-            
-            if (user == 'AnonymousUser'){
-	            console.log('User is not authenticated')
+		if (user == 'AnonymousUser'){
+			console.log('User is not authenticated')
 			
-            }else{
-	            console.log('User is authenticated, sending data...')
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
-        });
-    }
-});
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+                'Content-Type': 'application/json',
+                'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
